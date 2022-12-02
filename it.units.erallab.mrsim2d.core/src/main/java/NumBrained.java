a319ff4a171b1922a38a4cc7c6f76857afcbe915
Eric Medvet <eric.medvet@gmail.com>
2022-12-02 10:03:21 +0100

    Brained and multi-brained interfaces added.

---
+++ it.units.erallab.mrsim2d.core/src/main/java/NumBrained.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022 eric
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import it.units.erallab.mrsim2d.core.NumMultiBrained;
+import it.units.erallab.mrsim2d.core.functions.TimedRealFunction;
+
+import java.util.List;
+
+public interface NumBrained extends NumMultiBrained {
+
+  TimedRealFunction brain();
+
+  @Override
+  default List<TimedRealFunction> brains() {
+    return List.of(brain());
+  }
+}
