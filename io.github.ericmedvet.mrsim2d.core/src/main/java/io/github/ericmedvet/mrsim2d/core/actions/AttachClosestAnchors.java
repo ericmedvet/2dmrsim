f91ac2f732a6a58a603262e61d7853537b158de4
Eric Medvet <eric.medvet@gmail.com>
2022-12-28 19:18:19 +0100

    Bug fixed in finding of closest anchors.

--- io.github.ericmedvet.mrsim2d.core/src/main/java/io/github/ericmedvet/mrsim2d/core/actions/AttachClosestAnchors.java
+++ io.github.ericmedvet.mrsim2d.core/src/main/java/io/github/ericmedvet/mrsim2d/core/actions/AttachClosestAnchors.java
@@ -22,7 +22,6 @@ import io.github.ericmedvet.mrsim2d.core.SelfDescribedAction;
 import io.github.ericmedvet.mrsim2d.core.bodies.Anchor;
 import io.github.ericmedvet.mrsim2d.core.bodies.Anchorable;
 import io.github.ericmedvet.mrsim2d.core.engine.ActionException;
-import io.github.ericmedvet.mrsim2d.core.geometry.Point;
 
 import java.util.Collection;
 import java.util.Comparator;
@@ -39,15 +38,32 @@ public record AttachClosestAnchors(
 
   @Override
   public Collection<Anchor.Link> perform(ActionPerformer performer, Agent agent) throws ActionException {
-    Point targetCenter = Point.average(targetAnchorable
-        .anchors()
-        .stream()
-        .map(Anchor::point)
-        .toArray(Point[]::new));
-    return sourceAnchorable.anchors().stream()
-        .sorted(Comparator.comparingDouble(a -> a.point().distance(targetCenter)))
-        .limit(nOfAnchors)
-        .map(a -> performer.perform(new AttachAnchor(a, targetAnchorable, type), agent).outcome().orElseThrow())
+    //check how many anchors are already attached
+    Collection<Anchor> attached = sourceAnchorable().attachedTo(targetAnchorable());
+    //possibly attach new anchors
+    if (attached.size() < nOfAnchors) {
+      record AnchorPair(Anchor src, Anchor dst) {}
+      sourceAnchorable().anchors().stream()
+          .filter(a -> !attached.contains(a)) // remove already attached anchors
+          .map(a -> new AnchorPair(
+              a,
+              targetAnchorable().anchors().stream()
+                  .min(Comparator.comparingDouble(oa -> oa.point().distance(a.point())))
+                  .orElseThrow(() -> new IllegalArgumentException("Target anchorable has no anchors"))
+          )) // find closest dst anchor
+          .sorted(Comparator.comparingDouble(p -> p.src().point().distance(p.dst().point())))
+          .limit(nOfAnchors - attached.size())
+          .forEach(p -> performer.perform(
+              new CreateLink(p.src(), p.dst(), type),
+              agent
+          ));
+    }
+    return attached.stream()
+        .map(a -> a.links().stream()
+            .filter(l -> l.destination().anchorable().equals(targetAnchorable()))
+            .toList()
+        )
+        .flatMap(Collection::stream)
         .toList();
   }
 }
