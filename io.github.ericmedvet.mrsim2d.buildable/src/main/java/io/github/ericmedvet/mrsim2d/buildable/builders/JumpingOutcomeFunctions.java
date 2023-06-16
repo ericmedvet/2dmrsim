83623863c85d4c73a000617b1a2c97266e07bd71
Eric Medvet <eric.medvet@gmail.com>
2023-06-16 11:48:54 +0200

    Default value for transient time changed.

--- io.github.ericmedvet.mrsim2d.buildable/src/main/java/io/github/ericmedvet/mrsim2d/buildable/builders/JumpingOutcomeFunctions.java
+++ io.github.ericmedvet.mrsim2d.buildable/src/main/java/io/github/ericmedvet/mrsim2d/buildable/builders/JumpingOutcomeFunctions.java
@@ -28,37 +28,37 @@ public class JumpingOutcomeFunctions {
   }
 
   @SuppressWarnings("unused")
-  public static Function<Outcome, Double> avgY(
-      @Param(value = "transientTime", dD = 0) double transientTime
-  ) {
-    return o -> o.subOutcome(new DoubleRange(transientTime, o.duration())).firstAgentAverageY();
-  }
-
-  @SuppressWarnings("unused")
-  public static Function<Outcome, Double> maxY(
-      @Param(value = "transientTime", dD = 0) double transientTime
-  ) {
-    return o -> o.subOutcome(new DoubleRange(transientTime, o.duration())).firstAgentMaxY();
-  }
-
-  @SuppressWarnings("unused")
   public static Function<Outcome, Double> avgBBMinY(
-      @Param(value = "transientTime", dD = 0) double transientTime
+      @Param(value = "transientTime", dD = 5.0) double transientTime
   ) {
     return o -> o.subOutcome(new DoubleRange(transientTime, o.duration())).firstAgentAverageBBMinY();
   }
 
   @SuppressWarnings("unused")
+  public static Function<Outcome, Double> avgY(
+      @Param(value = "transientTime", dD = 5.0) double transientTime
+  ) {
+    return o -> o.subOutcome(new DoubleRange(transientTime, o.duration())).firstAgentAverageY();
+  }
+
+  @SuppressWarnings("unused")
   public static Function<Outcome, Double> maxBBMinY(
-      @Param(value = "transientTime", dD = 0) double transientTime
+      @Param(value = "transientTime", dD = 5.0) double transientTime
   ) {
     return o -> o.subOutcome(new DoubleRange(transientTime, o.duration())).firstAgentMaxBBMinY();
   }
 
   @SuppressWarnings("unused")
   public static Function<Outcome, Double> maxMaxRelJumpH(
-      @Param(value = "transientTime", dD = 0) double transientTime
+      @Param(value = "transientTime", dD = 5.0) double transientTime
   ) {
     return o -> o.subOutcome(new DoubleRange(transientTime, o.duration())).firstAgentMaxRelativeJumpHeight();
+  }
+
+  @SuppressWarnings("unused")
+  public static Function<Outcome, Double> maxY(
+      @Param(value = "transientTime", dD = 5.0) double transientTime
+  ) {
+    return o -> o.subOutcome(new DoubleRange(transientTime, o.duration())).firstAgentMaxY();
   }
 }
