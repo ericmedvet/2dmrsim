0141734a24b4cf1dad54ec31afd41c2c68ea428b
vittoriorussi <vittorio.russivr@gmail.com>
2024-12-10 10:13:52 +0100

    code cleaned

--- io.github.ericmedvet.mrsim2d.sample/src/main/java/io/github/ericmedvet/mrsim2d/sample/MyTester.java
+++ io.github.ericmedvet.mrsim2d.sample/src/main/java/io/github/ericmedvet/mrsim2d/sample/MyTester.java
@@ -30,10 +30,7 @@ import io.github.ericmedvet.mrsim2d.core.engine.Engine;
 import io.github.ericmedvet.mrsim2d.core.geometry.Terrain;
 import io.github.ericmedvet.mrsim2d.core.tasks.sumo.Sumo;
 import io.github.ericmedvet.mrsim2d.viewer.Drawer;
-import io.github.ericmedvet.mrsim2d.viewer.OnlineVideoBuilder;
 import io.github.ericmedvet.mrsim2d.viewer.RealtimeViewer;
-import io.github.ericmedvet.mrsim2d.viewer.VideoUtils;
-
 import java.io.*;
 import java.time.Duration;
 import java.time.Instant;
@@ -53,8 +50,8 @@ public class MyTester {
 
   private static Object fromBase64(String content) throws IOException {
     try (ByteArrayInputStream bais =
-                 new ByteArrayInputStream(Base64.getDecoder().decode(content));
-         ObjectInputStream ois = new ObjectInputStream(bais)) {
+            new ByteArrayInputStream(Base64.getDecoder().decode(content));
+        ObjectInputStream ois = new ObjectInputStream(bais)) {
       return ois.readObject();
     } catch (Throwable t) {
       throw new IOException(t);
@@ -65,30 +62,31 @@ public class MyTester {
     NamedBuilder<Object> nb = NamedBuilder.fromDiscovery();
     // prepare engine
     Supplier<Engine> engineSupplier =
-            () -> ServiceLoader.load(Engine.class).findFirst().orElseThrow();
+        () -> ServiceLoader.load(Engine.class).findFirst().orElseThrow();
     // do single task
     @SuppressWarnings("unchecked")
     Drawer drawer = ((Function<String, Drawer>)
             nb.build(
-                    "sim.drawer(framer = sim.staticFramer(minX = 10.0; maxX = 35.0; minY = 1.0; maxY = 15.0); actions = true)"))
-            .apply("test");
+                "sim.drawer(framer = sim.staticFramer(minX = 10.0; maxX = 35.0; minY = 1.0; maxY = 15.0); actions = true)"))
+        .apply("test");
     taskOn(
             nb,
             engineSupplier,
             new RealtimeViewer(30, drawer),
-//            "s.t.sumoArena()")
+            //            "s.t.sumoArena()")
             "s.t.flat()")
-            .run();
+        .run();
     System.exit(0);
-//        OnlineVideoBuilder onlineVideoBuilder = new OnlineVideoBuilder(300, 200, 0, 10, 20, VideoUtils.EncoderFacility.JCODEC, new File("video.mp4"), drawer);
-//        taskOn(
-//                nb,
-//                engineSupplier,
-//                //new RealtimeViewer(30, drawer),
-//                onlineVideoBuilder,
-//                "s.t.flat()")
-//                .run();
-//        onlineVideoBuilder.get();
+    //        OnlineVideoBuilder onlineVideoBuilder = new OnlineVideoBuilder(300, 200, 0, 10, 20,
+    // VideoUtils.EncoderFacility.JCODEC, new File("video.mp4"), drawer);
+    //        taskOn(
+    //                nb,
+    //                engineSupplier,
+    //                //new RealtimeViewer(30, drawer),
+    //                onlineVideoBuilder,
+    //                "s.t.flat()")
+    //                .run();
+    //        onlineVideoBuilder.get();
   }
 
   private static double profile(Runnable runnable, int nOfTimes) {
@@ -97,11 +95,11 @@ public class MyTester {
               Instant startingInstant = Instant.now();
               runnable.run();
               return Duration.between(startingInstant, Instant.now())
-                      .toMillis();
+                  .toMillis();
             })
             .average()
             .orElse(Double.NaN)
-            / 1000d;
+        / 1000d;
   }
 
   private static String readResource(String resourcePath) throws IOException {
@@ -118,7 +116,7 @@ public class MyTester {
   }
 
   private static Runnable taskOn(
-          NamedBuilder<?> nb, Supplier<Engine> engineSupplier, Consumer<Snapshot> consumer, String terrain) {
+      NamedBuilder<?> nb, Supplier<Engine> engineSupplier, Consumer<Snapshot> consumer, String terrain) {
     // prepare task
     Sumo sumo = new Sumo(30, (Terrain) nb.build(terrain));
     // read agent resource
@@ -175,9 +173,9 @@ public class MyTester {
       if (agent instanceof NumMultiBrained numMultiBrained) {
         //noinspection unchecked
         numMultiBrained.brains().stream()
-                .map(b -> Composed.shallowest(b, NumericalParametrized.class))
-                .forEach(o -> o.ifPresent(np ->
-                        np.setParams(params1.stream().mapToDouble(d -> d).toArray())));
+            .map(b -> Composed.shallowest(b, NumericalParametrized.class))
+            .forEach(o -> o.ifPresent(np -> np.setParams(
+                params1.stream().mapToDouble(d -> d).toArray())));
       }
       return agent;
     };
@@ -187,9 +185,9 @@ public class MyTester {
       if (agent instanceof NumMultiBrained numMultiBrained) {
         //noinspection unchecked
         numMultiBrained.brains().stream()
-                .map(b -> Composed.shallowest(b, NumericalParametrized.class))
-                .forEach(o -> o.ifPresent(np ->
-                        np.setParams(params2.stream().mapToDouble(d -> d).toArray())));
+            .map(b -> Composed.shallowest(b, NumericalParametrized.class))
+            .forEach(o -> o.ifPresent(np -> np.setParams(
+                params2.stream().mapToDouble(d -> d).toArray())));
       }
       return agent;
     };
